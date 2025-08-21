059ea68fb530e894b587e28d88293d1364bb5c77
sameeradissanayake <>
2025-08-21 17:35:46 +0800

    add streamlit file uploader

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from lekham!")
+import os
+import streamlit as st
 
 
-if __name__ == "__main__":
-    main()
+st.title("📚 LEKHAM")
+
+uploaded_file = st.file_uploader("upload doc", type=["pdf", "txt"])
+
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
