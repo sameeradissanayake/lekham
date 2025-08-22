2244bac6470fad382a71dfb8a979521087cc5d62
sameeradissanayake <>
2025-08-22 12:55:40 +0800

    update streamlit file uploader

--- app/main.py
+++ app/main.py
@@ -7,5 +7,9 @@ st.title("📚 LEKHAM")
 uploaded_file = st.file_uploader("upload doc", type=["pdf", "txt"])
 
 if uploaded_file is not None:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+    save_path = os.path.join("data/raw", uploaded_file.name)
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+    with open(save_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    st.success(f"saved file: {save_path}")
