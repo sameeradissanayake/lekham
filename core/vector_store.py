0cabaa0a26ba35e09a3e340c21d9f85152e00c2e
sameeradissanayake <>
2025-09-01 11:32:12 +0800

    update faiss similarity search

--- core/vector_store.py
+++ core/vector_store.py
@@ -1,17 +1,26 @@
 import faiss
 import numpy as np
-from typing import List,Dict
+from typing import List, Dict, Any
+
 
 
 class FaissHandler:
-    def __init__(self, dim: int):
+    def __init__(self, dim: int, metadata_list: List):
         self.index = faiss.IndexFlatL2(dim)
-        self.metadata: List[Dict] = []
+        self.metadata_list = metadata_list
 
 
-    def embedding_insert(self, embeddings):
+    def embedding_insert(self, embeddings: np.ndarray) -> None:
         if embeddings.dtype != np.float32:
             embeddings = embeddings.astype(np.float32)
 
         self.index.add(embeddings)
 
+
+    def similarity_search(self, search_query: str, embed_gen: Any, top_k: int = 3) -> List:
+        test_retreive_query = embed_gen.model.encode([search_query], convert_to_numpy=True).astype(np.float32)
+        distance, indices = self.index.search(test_retreive_query, top_k)
+
+        search_results = [{"text": self.metadata_list[index]["text"], "metadata": self.metadata_list[index]["metadata"]} for index in indices[0]]
+
+        return search_results
